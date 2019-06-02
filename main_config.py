5e6f81dc3bc417372f8f862d2c825aae2bf9d08d
pingaowang <pingaowang2015@gmail.com>
2019-06-02 16:16:23 -0400

    setup config: exp01_conv_3d_basic_ucf101

--- main_config.py
+++ main_config.py
@@ -2,11 +2,11 @@
 EXP_NAME = 'exp01_conv_3d_basic_ucf101'
 
 # Dataset config:
-DATA_FILE = 'data_file_mini.csv' # The original file is : 'data_file.csv'
+DATA_FILE = 'data_file.csv' # The original file is : 'data_file.csv'
 
 # Model config
 MODEL = 'conv_3d'
-BATCH_SIZE = 2
+BATCH_SIZE = 32
 
 # Checkpoint and Log config
 
