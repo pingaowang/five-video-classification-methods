8205a45a940dea79c1fa009e1d69b263a37fcae3
pingaowang <pingaowang2015@gmail.com>
2019-06-03 17:01:48 -0400

    add argments function. Currently only used for import cinfig file in command line.

---
+++ argments.py
@@ -0,0 +1,10 @@
+import argparse
+import os.path
+
+##-- args --##
+parser = argparse.ArgumentParser()
+parser.add_argument("config", type=str, help="config file's name")
+args = parser.parse_args()
+
+config_name = args.config
+CONFIG_PATH = os.path.join('config', config_name + '.txt') # txt???
--- main_config.py
+++ main_config.py
@@ -1,15 +1,14 @@
 # Experiment info config
-EXP_NAME = 'exp01_conv_3d_basic_ucf101'
+EXP_NAME = 'exp000_debug'
 
 # Load saved model config
 SAVED_MODEL = None# filepath=os.path.join('data', 'checkpoints', '')
 
 # Dataset config:
-DATA_FILE = 'data_file.csv' # The original file is : 'data_file.csv'
+DATA_FILE = 'data_file_mini.csv' # The original file is : 'data_file.csv'
 
 # Model config
 MODEL = 'conv_3d'
-BATCH_SIZE = 32
+BATCH_SIZE = 2
 
-# Checkpoint and Log config
 
