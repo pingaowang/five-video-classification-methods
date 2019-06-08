828def738518168afc203f31979ec4857cefe6e0
pingaowang <pingaowang2015@gmail.com>
2019-06-08 16:25:37 -0400

    Added exp group: 2nd reg 1e-5 to 1e-9

---
+++ exp_group/group008_2_reg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python train.py exp008_11_reg1e-5-ucf101-motion
+python train.py exp008_12_reg1e-6-ucf101-motion
+python train.py exp008_13_reg1e-7-ucf101-motion
+python train.py exp008_14_reg1e-8-ucf101-motion
+python train.py exp008_15_reg1e-9-ucf101-motion
+
