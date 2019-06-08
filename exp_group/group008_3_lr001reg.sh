17b5261ec3d54d476e024402ea3467b4652e7d98
pingaowang <pingaowang2015@gmail.com>
2019-06-08 17:58:54 -0400

    Added exp group: regularization with larger lr=0.01

---
+++ exp_group/group008_3_lr001reg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python train.py exp008_21_lr001reg1e-5-ucf101-motion
+python train.py exp008_22_lr001reg1e-6-ucf101-motion
+python train.py exp008_23_lr001reg1e-7-ucf101-motion
+python train.py exp008_24_lr001reg1e-8-ucf101-motion
+python train.py exp008_25_lr001reg1e-9-ucf101-motion
+
